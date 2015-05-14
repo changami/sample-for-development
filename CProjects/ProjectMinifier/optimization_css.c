fabd8a0dec93d6ca51c92ad46f4ee609b673ae95
chan_gami <babyryo@msn.com>
2015-05-13 23:00:02 -0700

    :new: create optimization_css.c and write comment of the implementation

---
+++ CProjects/ProjectMinifier/optimization_css.c
@@ -0,0 +1,18 @@
+int execOptimizationCSS(char *HTMLFilePath, char *CSSFilePath, char *newCSSFilePathToCreate) {
+    // Open HTML file (READING_MODE) As htmlOrg
+    // Open CSS file (READING_MODE) As cssOrg
+    // Open new CSS file (CREATING_MODE) As cssNew
+
+    // ------- Loop -------
+    // Cursor moves to first in HTML
+    // Find a class/id/tag name in CSS
+    // Check whether those are used in HTML
+    //     (If those are used in HTML,)
+    //     Extract the target block from CSS
+    //     Write the target block to new CSS file
+    // ------- End --------
+
+    // Close all files
+
+    return 0;
+}
